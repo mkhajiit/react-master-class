ad6aa1b07733c5b28e7b9558f2ab260675fcfd2d
mkhajiit <mkhajiit15@gmail.com>
2024-01-31 15:41:42 +0900

    styled-component 연습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
+  const Father = styled.div`
+    display: flex;
+  `;
+  const Box = styled.div`
+    background-color: ${(props) => props.bgColor};
+    width: 100px;
+    height: 100px;
+  `;
+
+  const Circle = styled(Box)`
+    border-radius: 50px;
+  `;
+  const Text = styled.span`
+    color: white;
+  `;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box bgColor='tomato'>
+        <Text>Hello</Text>
+      </Box>
+      <Box bgColor='teal'></Box>
+      <Circle bgColor='blue'></Circle>
+    </Father>
   );
 }
 
